16c8be050eff9dcf9a61f630081209e3216687e8
serkantoraman <serkan@github.com>
2023-08-22 21:14:21 +0300

    Init commit

---
+++ src/test/java/com/workintechS21G2/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintechS21G2.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
